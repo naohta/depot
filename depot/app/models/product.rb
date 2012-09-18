d99d636ec62df5581dd2e7aa0e55c26ffa125aae
naohta <naohta@gmail.com>
2012-09-18 16:25:27 +0900

    bug detected! now comment out it. on models/product.rb

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -3,8 +3,8 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence:true
   validates :price, numericality:{greater_than_or_equal_to:0.01}
   validates :title, uniqueness:true
-  validates :image_url, allow_blank:true, format:{
-    with: %r{\.(gif|jpg|png)$}1,
-    message: ' must be url to gif, jpg, png.'
-  }
+#  validates :image_url, allow_blank:true, format:{
+#    with: %r{\.(gif|jpg|png)$}1,
+#    message: ' must be url to gif, jpg, png.'
+#  }
 end
