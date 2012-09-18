927ea3d263bf7d6bb7732c74cb292bbd88a3b4bc
naohta <naohta@gmail.com>
2012-09-18 17:29:58 +0900

    page 81 of the book

--- depot/test/unit/product_test.rb
+++ depot/test/unit/product_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    p = Product.new
+    assert p.invalid?
+    assert p.errors[:title].any?
+    assert p.errors[:description].any?
+    assert p.errors[:price].any?
+    assert p.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    p = Product.new(title:"My Book Title",
+      description:"yyy",
+      image_url:"zzz.jpg")
+
+    p.price = -1
+    assert p.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      p.errors[:price].join('; ')
+    
+    p.price = 0
+    assert p.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      p.errors[:price].join('; ')
+    
+    p.price = 1
+    assert p.valid?
+  end
 end
