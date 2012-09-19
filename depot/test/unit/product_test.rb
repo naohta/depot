11ab0851d93351038621202a7f9cde3d97f6c656
naohta <naohta@gmail.com>
2012-09-19 13:48:57 +0900

    page 86 of the book

--- depot/test/unit/product_test.rb
+++ depot/test/unit/product_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures:products
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(
+      title: products(:ruby).title,
+      description: "yyy",
+      price: 1,
+      image_url: "fred.gif"
+    )
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+      product.errors[:title].join('; ')
+  end
+
   test "product attributes must not be empty" do
     p = Product.new
     assert p.invalid?
