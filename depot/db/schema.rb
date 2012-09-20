234093e43971933aff4a8aa0dbeb581ab1b34858
naohta <naohta@gmail.com>
2012-09-20 11:04:03 +0900

    rake db:migrate

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120918033308) do
+ActiveRecord::Schema.define(:version => 20120920015829) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
