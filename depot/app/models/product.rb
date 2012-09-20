2b2f3fff162bc0ac03c827f785c340237556ca88
naohta <naohta@gmail.com>
2012-09-20 17:27:26 +0900

    mm...

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   attr_accessible :description, :image_url, :price, :title
+  
   validates :title, :description, :image_url, presence:true
   validates :price, numericality:{greater_than_or_equal_to:0.01}
   validates :title, uniqueness:true
@@ -8,9 +12,6 @@ class Product < ActiveRecord::Base
     message: ' must be url to gif, jpg, png.'
   }
 
-  has_many :line_items
-  before_destroy :ensure_not_referenced_by_any_line_item
-  
   private
     def ensure_not_referenced_by_any_line_item
       if line_items.empty?
@@ -18,6 +19,7 @@ class Product < ActiveRecord::Base
       else
         errors.add(:base, 'There are line_item(s) that refer this product')
 	return false
+      end
     end
 
 end
